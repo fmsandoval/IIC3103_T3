0406fc711b15f75987e5b65f0b5399be728fb291
Fernando Sandoval <fmsandoval@uc.cl>
2018-05-27 22:25:26 -0400

    integrando socket.io

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
 class App extends Component {
+  componentDidMount() {
+    this.socket = io('wss://integracion-tarea-3.herokuapp.com', {
+      path: '/flights',
+      transports: ['websocket']
+    });
+    this.socket.on('POSITION', function (data) {
+      console.log(data);
+    });
+    this.socket.on('AIRPORTS', function (data) {
+      console.log(data);
+    });
+    this.socket.on('FLIGHTS', function (data) {
+      console.log(data);
+    });
+  }
+
+  requestAirports() {
+    this.socket.emit("AIRPORTS");
+  }
+
+  requestFlights() {
+    this.socket.emit("FLIGHTS");
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +38,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.requestAirports.bind(this)}>
+          Airports
+        </button>
+        <br/><br/>
+        <button onClick={this.requestFlights.bind(this)}>
+          Flights
+        </button>
       </div>
     );
   }
